ded7804f193679e9e2f241e3b623fba47b9493a2
alimranhossain <alimranhossain44@gmail.com>
2022-11-17 21:20:03 +0600

    server side

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6xivgke.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
+        const bookingsCollection = client.db('doctorsPortal').collection('booking');
+        
+        //use aggregate to query multiple collection and then merge data
+        app.get('/appointmentOptions', async(req, res)=>{
+            const date = req.query.date;
+            console.log(date);
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+
+            //get the bookings of the provider date
+            const bookingQuery = {appointmentDate: date}
+            const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
+
+            // find slot available 
+            options.forEach(option =>{
+                const optionBooked = alreadyBooked.filter(book => book.treatment === option.name);
+                const bookedSlots = optionBooked.map(book => book.slot);
+                const remainingSlot = option.slots.filter(slot => !bookedSlots.includes(slot))
+                option.slots = remainingSlot;
+            })
+            res.send(options);
+        })
+     
+        app.post('/bookings', async(req, res)=>{
+            const booking = req.body;
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+            const alreadyBooked =await bookingsCollection.find(query).toArray();
+
+            if(alreadyBooked.length){
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({acknowledged: false, message})
+            }
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        })
+    }
+    finally{
+
+    }
+}
+// run().catch(console.log);
+run().catch((err) => console.error(err));
+
+app.get('/', async(req, res)=>{
+    res.send('doctors portal server is running')
+})
+
+app.listen(port, ()=> console.log(`Doctors porlat runiing on port: ${port}`))
